b0ac7f37b31610cd435de8884c8015b6bf4159c2
TiltRex <afls2002@gmail.com>
2023-10-23 22:07:06 -0500

    Front Update

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ let images = [
   "dice-05.svg",
   "dice-06.svg"
 ];
+
+let images2 = [
+  "dice2-01.svg",
+  "dice2-02.svg",
+  "dice2-03.svg",
+  "dice2-04.svg",
+  "dice2-05.svg",
+  "dice2-06.svg"
+];
+
 let diceWrappers = document.querySelectorAll(".dado_container"); // Elementos con clase .dice-wrapper
 
 function roll() {
@@ -29,7 +39,7 @@ function roll() {
     let dieTwoValue = Math.floor(Math.random() * 6);
     console.log(dieOneValue, dieTwoValue);
     document.querySelector("#dado_1").setAttribute("src", images[dieOneValue]);
-    document.querySelector("#dado_2").setAttribute("src", images[dieTwoValue]);
+    document.querySelector("#dado_2").setAttribute("src", images2[dieTwoValue]);
 
       // Guardar los resultados de los dados en las listas
       dadoTiempo.push(dieOneValue + 1);
@@ -38,13 +48,24 @@ function roll() {
       console.log("dadoTiempo: " + dadoTiempo);
       console.log("dadoUbicacion: " + dadoUbicacion);
 
+      document.querySelector("#mensaje-tiempo").textContent = "";
+      document.querySelector("#mensaje-ubicacion").textContent = "";    
       document.querySelector("#mensaje-avance").textContent = "";
+      document.querySelector("#ecuacion-velocidad").textContent = "";
+      
+
+
 //avance
 
 
   let mensaje = "";
+  let ecuacion = "V = (x2 - x1) / (t2 - t1)";
+  let datosTiempo = "t: ";
+  let datosUbicacion = "x: ";
   let indice1 = dadoUbicacion.length - 1;
   let indice2 = dadoUbicacion.length - 2; 
+  let indice3 = dadoTiempo.length - 1;
+  let indice4 = dadoTiempo.length - 2; 
   if(dadoTiempo.length < 2 && dadoUbicacion.length < 2){
     
     mensaje = "Lanzar otra vez";
@@ -53,18 +74,27 @@ function roll() {
     if(((dadoTiempo[1]) - (dadoTiempo[0])) === 0){
       mensaje = "Quédate quieto";
     }else{
-      let velocidad = ((dadoUbicacion[indice1] - dadoUbicacion[indice2]) / (dadoTiempo[indice1] - dadoTiempo[indice2]));
+      let velocidad = ((dadoUbicacion[indice1] - dadoUbicacion[indice2]) / (dadoTiempo[indice3] - dadoTiempo[indice4]));
+      let mover = velocidad + dadoUbicacion[indice1] + dadoTiempo[indice3];
       let apro = Math.round(velocidad);
-      if (apro === Infinity || apro === -Infinity) {
-        mensaje = "Quédate quieto";
+      if (velocidad === Infinity || velocidad === -Infinity) {
+        ecuacion = "V = " + "(" + dadoUbicacion[indice1] + " - " + dadoUbicacion[indice2] + ") / (" + dadoTiempo[indice3] + " - " + dadoTiempo[indice4] + ") = " + velocidad;
+        let mov = dadoUbicacion[indice1]  + dadoTiempo[indice3];
+        mensaje = "Avanza " + mov + " casillas.";
       } else if (apro < 0) {
-        apro = Math.abs(apro);
-        mensaje = "Retrocede " +  apro + " casillas";
+        ecuacion = "V = " + "(" + dadoUbicacion[indice1] + " - " + dadoUbicacion[indice2] + ") / (" + dadoTiempo[indice3] + " - " + dadoTiempo[indice4] + ") = " + velocidad;
+        let mov2 = dadoUbicacion[indice1] + dadoTiempo[indice3] + Math.abs(velocidad);
+        mov2 = Math.abs(mov2);
+        mov2 = Math.round(mov2);
+        mensaje = "Retrocede " +  mov2 + " casillas." + "\n" + "Tu velocidad fue de: " + apro;
       } else if (apro > 0) {
-        apro = Math.abs(apro);
-        mensaje = "Avanza " +  apro + " casillas";
+        ecuacion = "V = " + "(" + dadoUbicacion[indice1] + " - " + dadoUbicacion[indice2] + ") / (" + dadoTiempo[indice3] + " - " + dadoTiempo[indice4] + ") = " + velocidad;
+        mover = Math.abs(mover);
+        mover = Math.round(mover);
+        mensaje = "Avanza " +  mover + " casillas." + "\n" + "Tu velocidad fue de: " + apro;
       } else {
-        mensaje = "Quédate quieto";
+        ecuacion = "V = " + "(" + dadoUbicacion[indice1] + " - " + dadoUbicacion[indice2] + ") / (" + dadoTiempo[indice3] + " - " + dadoTiempo[indice4] + ") = " + velocidad;
+        mensaje = "Quédate quieto tu velocidad fue de: " + apro;
       }
 
     }
@@ -73,7 +103,21 @@ function roll() {
 
   }
 
+  for (var i = 0; i < dadoTiempo.length; i++) {
+    datosTiempo += dadoTiempo[i];
+    datosUbicacion += dadoUbicacion[i];
+    if (i < dadoUbicacion.length - 1) {
+      datosTiempo += ", ";
+      datosUbicacion += ", ";
+    }
+  }
+
+  document.querySelector("#mensaje-tiempo").textContent = datosTiempo;
+  document.querySelector("#mensaje-ubicacion").textContent = datosUbicacion;
+
+  document.querySelector("#ecuacion-velocidad").textContent = ecuacion;
   document.querySelector("#mensaje-avance").textContent = mensaje;
+
 
 
   }, 1000);
